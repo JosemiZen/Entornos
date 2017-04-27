d2d0acd0a5f7eed4160cd17af2c7cc192cff53d3
Josemi Gonzalez <xemy.013@gmail.com>
2017-04-27 10:25:32 +0200

    prueba para la actualizacion en otros workingspaces

---
+++ src/prueba1/Prueba2.java
@@ -0,0 +1,18 @@
+package prueba1;
+
+public class Prueba2 {
+	public static void main(String[] args) {
+		int suma, producto, i;
+		suma = 0;
+		producto = 1;
+		for (i = 1; i <= 40; i++) {
+			if (i % 2 == 0) {
+				suma = suma + i;
+			} else {
+				producto = producto * i;
+			}
+		}
+		System.out.println("La suma de los pares es " + suma);
+		System.out.println("El producto de los impares es " + producto);
+	}
+}
